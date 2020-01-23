c0f1e3d65314c66772ebd560c3cd2d88aa9bca84
masaroto <pedrolandins@gmail.com>
2020-01-23 20:16:17 -0300

    Update trab2.py

--- trab2.py
+++ trab2.py
@@ -4,7 +4,7 @@ from bisect import insort
 # random.seed(379126485)
 
 class Vertex():
-	def __init__(self, coord_x, coord_y, capacity_max, demand):
+	def __init__(self ,coord_x, coord_y, capacity_max, demand):
 		self.coord_x = coord_x
 		self.coord_y = coord_y
 		self.capacity_max = capacity_max
@@ -15,6 +15,9 @@ class Vertex():
 		self.connected_vertices = []
 		self.capacity_current = demand
 	
+	def __str__(self):
+		return "Vertice(%d, %d)" % (self.coord_x, self.coord_y)
+
 	def __lt__(self, other):
 		self.distance < other.distance
 
@@ -24,10 +27,17 @@ class Solution():
 		self.nVertices = nVertices
 		self.vertices = vertices
 		self.fitness = fitness
-
+	
 	def __lt__(self, other):
 		self.fitness < other.fitness
 
+def printMedians(median_list):
+	for x in median_list:
+			print("=========================")
+			print("Median: ", x)
+			for y in x.connected_vertices:
+				print(y)
+			print("=========================")
 
 def calcDist(median,vertex):
 	x1 = median.coord_x
@@ -40,28 +50,39 @@ def selectMedians(vertex_list, n_medians):
 	median_list = []
 	for i in range(n_medians):
 		median = vertex_list.pop(0)
-		median.isConnected = True
+		# median.isConnected = True
 		median_list.append(median)
 	return median_list
 
-def connectVertexToMedian(median, vertex):
+def connectVertexToMedian(median, vertex, dist):
 	median.connected_vertices.append(vertex)
 	median.capacity_current += vertex.demand
+	vertex.distance = dist
 	vertex.isConnected = True
 
 def makeGraph(n_vertices, n_medians, vertex_list, median_list):
 	for median in median_list:
 		distList = []
 		for vertex in vertex_list:
-			if median.capacity_current + vertex.demand <= median.capacity_max:
+			if not vertex.isConnected:
 				insort(distList,(calcDist(median, vertex), vertex))
-		while len(distList) > 0 \
-			and median.capacity_current < median.capacity_max:
+		
+		for x in distList:
+			if median.capacity_current + x[1].demand <= median.capacity_max:
+				connectVertexToMedian(median, x[1], x[0])
+		# while len(distList) > 0 \
+		# 	and median.capacity_current < median.capacity_max:
 			
-			closest = distList.pop(0)
-			if not closest[1].isConnected:
-				closest[1].distance = closest[0]
-				connectVertexToMedian(median, closest[1])
+		# 	closest = distList.pop(0)
+		# 	if not closest[1].isConnected:
+		# 		closest[1].distance = closest[0]
+		# 		connectVertexToMedian(median, closest[1])
+		
+	for x in vertex_list:
+		if not vertex.isConnected:
+			return -1
+	return median_list
+
 		
 
 def addDist(vec):
@@ -76,11 +97,10 @@ def randomSol(n_vertices, n_medians, original_vertex_list):
 
 	random.shuffle(vertex_list)
 	median_list = selectMedians(vertex_list, n_medians)
-
 	aux = -1
 	while aux == -1:
-		aux = makeGraph(n_vertices, n_medians, median_list, vertex_list)
-
+		aux = makeGraph(n_vertices, n_medians, vertex_list, median_list)
+	
 	return Solution(n_medians, n_vertices, vertex_list, addDist(vertex_list))
 
 
@@ -88,6 +108,7 @@ def randomPopulation(n_vertices, n_medians, vertex_list):
 	solutionList = []
 	for i in range(0, int(7.5 * math.log(n_medians))):
 		solution = randomSol(n_vertices, n_medians, vertex_list)
+		print(solution.fitness)
 		insort(solutionList, solution)
 	for solution in solutionList:
 		print(solution.fitness)
@@ -108,6 +129,9 @@ if __name__ == "__main__":
 		vertex_list.append(new_vertex)
 	
 	randomPopulation(n_vertices, n_medians, vertex_list)
+	# randomSol(n_vertices, n_medians, vertex_list)
+	# makeGraph(n_vertices, n_medians, vertex_list, )
+
 	
 
 
